6efbc9f4c64d5ae0229429df84e3448b26fa1393
Trenton <tplager1@gmail.com>
2019-10-12 14:17:15 -0400

    Think logic for round pieces should be working now

--- Assets/Scripts/BlockMovement.cs
+++ Assets/Scripts/BlockMovement.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections.Generic; 
 
 public class BlockMovement : MonoBehaviour
 {
@@ -41,15 +40,56 @@ public class BlockMovement : MonoBehaviour
     {
         if (clicked)
         {
-            Vector3 mouseCurrentPosition; 
-            //if (mousePreviousPosition.x < mouseCurrentPosition.x)
+            Vector3 centerToBlock = (transform.position - Vector3.zero).normalized;
+            Vector3 right = new Vector3(centerToBlock.z, centerToBlock.y, -centerToBlock.x);
+            Vector3 mouseCurrentPosition = Input.mousePosition;
+            mouseCurrentPosition = Camera.main.ScreenToWorldPoint(mouseCurrentPosition); 
+            float dotProduct = Vector3.Dot(gameObject.transform.right, mouseCurrentPosition);
+            float mag = (mouseCurrentPosition - gameObject.transform.position).magnitude; 
+
+            Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + gameObject.transform.right);
+            Debug.DrawLine(Vector3.zero, mouseCurrentPosition); 
+
+            if (dotProduct < 0)
             {
-                //Quaternion rotation = gameObject.transform.eulerAngles;
-                //rotation.z -= speed;
-                //gameObject.transform.rotation = rotation; 
+                Vector3 rotation = gameObject.transform.eulerAngles;
+                rotation.z += speed;
+                gameObject.transform.eulerAngles = rotation;
+
+                if (gameObject.transform.eulerAngles.z > 360)
+                {
+                    Vector3 fixedRotation = gameObject.transform.eulerAngles;
+                    fixedRotation.z = fixedRotation.z - 360;
+                    gameObject.transform.eulerAngles = fixedRotation;
+                }
             }
+            else
+            {
+                Vector3 rotation = gameObject.transform.eulerAngles;
+                rotation.z -= speed;
+                gameObject.transform.eulerAngles = rotation;
+
+                if (gameObject.transform.eulerAngles.z < 0)
+                {
+                    Vector3 fixedRotation = gameObject.transform.eulerAngles;
+                    fixedRotation.z = 360 + fixedRotation.z;
+                    gameObject.transform.eulerAngles = fixedRotation;
+                }
+            }
+            //if (mousePreviousPosition.x < mouseCurrentPosition.x || mousePreviousPosition.y < mouseCurrentPosition.y)
+            //{
+            //    Vector3 rotation = gameObject.transform.eulerAngles;
+            //    rotation.z -= speed;
+            //    gameObject.transform.eulerAngles = rotation;
+            //}
+            //else if (mousePreviousPosition.x > mouseCurrentPosition.x || mousePreviousPosition.y > mouseCurrentPosition.y)
+            //{
+            //    Vector3 rotation = gameObject.transform.eulerAngles;
+            //    rotation.z += speed;
+            //    gameObject.transform.eulerAngles = rotation;
+            //}
+            mousePreviousPosition = mouseCurrentPosition; 
         }
-        //mousePreviousPosition = mouseCurrentPosition; 
     }
 
     public void StraightMovement()
@@ -68,6 +108,36 @@ public class BlockMovement : MonoBehaviour
 
     void OnMouseUp()
     {
-        clicked = false; 
+        clicked = false;
+
+        List<float> rotationDiffs = new List<float>(snapLocations.Length); 
+
+        for (int i = 0; i < snapLocations.Length; i++)
+        {
+            rotationDiffs.Add(Mathf.Abs(transform.eulerAngles.z - snapLocations[i].transform.eulerAngles.z)); 
+        }
+
+        int smallestDiffInd = 0; 
+
+        for (int i = 1; i < snapLocations.Length; i++)
+        {
+            if (rotationDiffs[i] < rotationDiffs[smallestDiffInd])
+            {
+                smallestDiffInd = i; 
+            }
+        }
+
+        if (Mathf.Abs(transform.eulerAngles.z - 360) < rotationDiffs[smallestDiffInd])
+        {
+            Vector3 rotation = gameObject.transform.eulerAngles;
+            rotation.z = 0;
+            gameObject.transform.eulerAngles = rotation;
+        }
+        else
+        {
+            Vector3 rotation = gameObject.transform.eulerAngles;
+            rotation.z = snapLocations[smallestDiffInd].transform.eulerAngles.z;
+            gameObject.transform.eulerAngles = rotation;
+        }
     }
 }
---
+++ Assets/Scripts/PathCheck.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathCheck : MonoBehaviour
+{
+    #region Fields
+    [SerializeField]
+    private bool[] validPaths = new bool[4]; //an array indicating where this path can go
+    private string layerMaskName;
+    private string layerUpMaskName;
+    private string layerDownMaskName; 
+    [SerializeField]
+    private Material bloodMaterial;
+    [SerializeField]
+    private Material pathMaterial;
+    [SerializeField]
+    private bool validPath; 
+    #endregion
+
+    public bool[] ValidPaths { get { return validPaths; } }
+    public bool ValidPath { get { return validPath; } set { validPath = value; } }
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.layer < 11)
+        {
+            layerUpMaskName = LayerMask.LayerToName(gameObject.layer + 1);
+        }
+        layerDownMaskName = LayerMask.LayerToName(gameObject.layer - 1);
+        layerMaskName = LayerMask.LayerToName(gameObject.layer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (layerMaskName != "CenterLayer")
+        {
+            validPath = false;
+        }
+
+        //if (validPaths[0])
+        //{
+        //    CheckPathsInFront();
+        //}
+        if (validPaths[1])
+        {
+            CheckPathsRight();
+        }
+        if (validPaths[2])
+        {
+            CheckPathsBehind();
+        }
+        if (validPaths[3])
+        {
+            CheckPathsLeft();
+        }
+
+        if (validPath)
+        {
+            ChangeToBloodMaterial(); 
+        }
+        else
+        {
+            ChangeToEmptyPathMaterial(); 
+        }
+    }
+
+    private void CheckPathsInFront()
+    {
+        RaycastHit hit; 
+        bool isSomethingInFront = Physics.Raycast(gameObject.transform.position, gameObject.transform.up, out hit, 50, LayerMask.GetMask(layerUpMaskName));
+        if (isSomethingInFront) { Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + (gameObject.transform.up), Color.red); }
+        else { Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + (gameObject.transform.up), Color.black); }
+
+        if (isSomethingInFront)
+        {
+            if (hit.collider.gameObject.GetComponent<PathCheck>().ValidPaths[2] && hit.collider.gameObject.GetComponent<PathCheck>().ValidPath)
+            {
+                validPath = true;
+            }
+        }
+    }
+
+    private void CheckPathsBehind()
+    {
+        RaycastHit hit;
+        bool isSomethingBehind = Physics.Raycast(gameObject.transform.position, gameObject.transform.up, out hit, 50, LayerMask.GetMask(layerDownMaskName));
+        if (isSomethingBehind) { Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + (gameObject.transform.up), Color.red); }
+        else { Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + (gameObject.transform.up), Color.black); }
+
+        if (isSomethingBehind)
+        {
+            if (hit.collider.gameObject.GetComponent<PathCheck>().ValidPaths[0] && hit.collider.gameObject.GetComponent<PathCheck>().ValidPath)
+            {
+                validPath = true;
+            }
+        }
+    }
+
+    private void CheckPathsLeft()
+    {
+        RaycastHit hit;
+        Quaternion rotation = Quaternion.Euler(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z + 45);
+
+        bool isSomethingLeft = Physics.Raycast(gameObject.transform.position, rotation * gameObject.transform.up, out hit, 50, LayerMask.GetMask(layerMaskName));
+        if (isSomethingLeft) { Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + (rotation * gameObject.transform.up), Color.red); }
+        else { Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + (rotation * gameObject.transform.up), Color.black); }
+
+        if (isSomethingLeft)
+        {
+            if (hit.collider.gameObject.GetComponent<PathCheck>().ValidPaths[1] && hit.collider.gameObject.GetComponent<PathCheck>().ValidPath)
+            {
+                validPath = true;
+            }
+        }
+    }
+
+    private void CheckPathsRight()
+    {
+        RaycastHit hit;
+
+        Quaternion rotation = Quaternion.Euler(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z - 45);
+
+        bool isSomethingRight = Physics.Raycast(gameObject.transform.position, rotation * gameObject.transform.up, out hit, 50, LayerMask.GetMask(layerMaskName));
+        if (isSomethingRight) { Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + (rotation * gameObject.transform.up), Color.red); }
+        else { Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + (rotation * gameObject.transform.up), Color.black); }
+
+        if (isSomethingRight)
+        {
+            if (hit.collider.gameObject.GetComponent<PathCheck>().ValidPaths[3] && hit.collider.gameObject.GetComponent<PathCheck>().ValidPath)
+            {
+                validPath = true;
+            }
+        }
+    }
+
+    public void ChangeToBloodMaterial()
+    {
+        gameObject.GetComponentInChildren<MeshRenderer>().material = bloodMaterial; 
+    }
+
+    public void ChangeToEmptyPathMaterial()
+    {
+        gameObject.GetComponentInChildren<MeshRenderer>().material = pathMaterial;
+    }
+}
