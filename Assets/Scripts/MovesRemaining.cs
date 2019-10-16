a6d73abf746c713bd358588ba0dcf113433ce237
Darius James <daj7160@g.rit.edu>
2019-10-16 19:57:10 -0400

    Started UI Work
    Moves change when you click, score implementation still needs to be worked on. Changed Size of assets to work with UI.

---
+++ Assets/Scripts/MovesRemaining.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovesRemaining : MonoBehaviour
+{
+    public int movesRemaining;
+    public Text movesText;
+
+    void Update()
+    {
+        movesText.text = "Moves Remaining: " + movesRemaining;
+        if(Input.GetMouseButtonDown(0))
+        {
+            movesRemaining--;
+        }
+    }
+}
