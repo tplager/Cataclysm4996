f2319496e8bed23537d2d63d50c2193a81d0d85b
Darius James <daj7160@g.rit.edu>
2019-10-22 18:18:37 -0400

    Added quick How To Play screen as a temporary onboarding solution

--- Assets/Scripts/TutorialText.cs
+++ Assets/Scripts/TutorialText.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialText : MonoBehaviour
 {
+    public Text tutorial;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine("Hello, welcome to Cataclysm 4996", 5.0f);
+        //StartCoroutine("Click and Drag on top of a piece to move it", 5.0f);
+        //StartCoroutine("Make a path from the red center to the white piece outside of the circle to complete the level", 5.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
+    }
+
+    IEnumerator ShowMessage(string message, float delay)
+    {
+        tutorial.text = message;
+        tutorial.enabled = true;
+        yield return new WaitForSeconds(delay);
+        tutorial.enabled = false;
     }
 }
