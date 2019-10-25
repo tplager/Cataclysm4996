da3c661a51f6f2f084fd75a8608b3dde5a40054d
Darius James <daj7160@g.rit.edu>
2019-10-25 03:15:12 -0400

    Pause Menu rolling out slowly

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                ResumeGame(); 
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Resuming");
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        paused = false;
+    }
+
+
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        paused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
